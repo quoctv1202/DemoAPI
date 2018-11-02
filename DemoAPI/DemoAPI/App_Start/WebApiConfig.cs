cae2c69dcbea36437e86e8eb5c16a297cd104d74
quoctv1202 <quoctv1202@gmail.com>
2018-11-02 10:26:50 +0700

    demo StudentAPI

---
+++ DemoAPI/DemoAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace DemoAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
